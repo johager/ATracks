c6f3bc1e1876712cd450e8c335a0f1f2f4220898
James Hager <james@avantiapplications.com>
2022-04-18 08:29:27 -0600

    Initial Commit

---
+++ Shared/ATracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATracksApp.swift
+//  Shared
+//
+//  Created by James Hager on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct ATracksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
