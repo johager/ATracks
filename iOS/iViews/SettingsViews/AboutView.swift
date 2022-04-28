607cddc76cfca556f346812fab33d5d9bd669e89
James Hager <james@avantiapplications.com>
2022-04-28 06:37:55 -0600

    i29f - Add About and Legal text.

--- iOS/iViews/SettingsViews/AboutView.swift
+++ iOS/iViews/SettingsViews/AboutView.swift
@@ -9,14 +9,51 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("ATracks...")
-                Spacer()
+        List() {
+            Text("ATracks records location, elevation, and step data for your outdoor activities. Data is stored locally so that you'll always have it, and data is synched to other devices with the same Apple ID so you can view your excursions on larger screens.")
+                .listRowSeparator(.hidden)
+            Text("Your data is stored in your own private iCloud account, and no one without access to your Apple ID can access that data.")
+                .listRowSeparator(.hidden)
+            
+            Section {
+                Text("The location and elevation data is obtained from the GPS in your device. You must allow ATracks to use your location. The app will first ask permission to \"Allow Once\" or \"Allow While Using App\". Select \"Allow While Using App\" so that your track can be followed. This will only allow tracking while the app is active. After you grant permission for \"Allow While Using App\" and start tracking, exit the app so that you can be prompted to allow background tracking by selecting \"Change to Always Allow.\" If you don't allow background tracking, the app will not provide accurate locations while you are tracking your activity if the app is not active.")
+                Text("The steps data is obtained from the Apple Health app, and you must allow ATracks to read \"Steps.\"")
+            } header: {
+                Text("Set Up")
+                    .font(.title3)
+                    .bold()
+                    .foregroundColor(.headerText)
+                    .kerning(1)
+                    .textCase(.uppercase)
             }
-            Spacer()
+            .listRowSeparator(.hidden)
+            
+            Section {
+                Text("The app can be set up to require that you \"Start\" and \"Stop\" tracking each excursion or have the app Auto-Stop the tracking.")
+                Text("Auto-Stop will stop the tracking if you are more than 20 yds away from your starting point more than 30 seconds after starting to track, and then return within 8 yds of your starting point.")
+            } header: {
+                Text("Tracking - Auto Stop")
+                    .font(.title3)
+                    .bold()
+                    .foregroundColor(.headerText)
+                    .kerning(1)
+                    .textCase(.uppercase)
+            }
+            .listRowSeparator(.hidden)
+            
+            Section {
+                Text("The step data is not available from the Health App immediately, so there is always a lag in the data. Final data is usually available 24 hours after completing your activity.")
+            } header: {
+                Text("Steps")
+                    .font(.title3)
+                    .bold()
+                    .foregroundColor(.headerText)
+                    .kerning(1)
+                    .textCase(.uppercase)
+            }
+            .listRowSeparator(.hidden)
         }
-        .padding()
+        .listStyle(.plain)
         .navigationTitle("About")
         #if os(iOS)
         .navigationBarTitleDisplayMode(.inline)
--- iOS/iViews/SettingsViews/LegalView.swift
+++ iOS/iViews/SettingsViews/LegalView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct LegalView: View {
+    
+    private let itemInfo: [[String]] = [
+        ["ATracks", "The tracking information provided by ATracks is for recreational purposes, and is offered without any warranties express or implied. Avanti Applications, LLC does not guarantee the accuracy or availability of the data, and shall not be held liable for any errors in the data."],
+        ["Graphics Images", "Some images are from www.flaticon.com and were designed by Smashicons."]
+    ]
+    
     var body: some View {
-        List {
-            Text("Legal...")
+        List() {
+            ForEach(0..<itemInfo.count, id: \.self) { index in
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(itemInfo[index][0])
+                        .font(.headline)
+                    Text(itemInfo[index][1])
+                        .font(.footnote)
+                }
+//                Text(itemInfo[$0][0])
+//                    .font(.headline)
+            }
         }
         .listStyle(.plain)
         .navigationTitle("Legal")
