8102e3bba56d2be4775198f1fb23cb7ceedea8f1
James Hager <james@avantiapplications.com>
2022-05-02 15:05:17 -0600

    i31d, m26d - 1) New AppIcon. 2) Mod About page to include more info about steps and to include info about starting a track.

--- iOS/iViews/SettingsViews/AboutView.swift
+++ iOS/iViews/SettingsViews/AboutView.swift
@@ -56,6 +56,7 @@ struct AboutView: View {
                     Text("The location and elevation data is obtained from the GPS in your device. You must allow ATracks to track your location. Location access \"While Using the App\" will allow ATracks to track your location only when the app is active, and \"Always\" will allow ATracks to track your location while the app is in the background.")
                     Text("The app will first ask permission to \"Allow Once\" or \"Allow While Using App\". Select \"Allow While Using App\" so that your track can be followed. This will only allow tracking while the app is active. After you grant permission for \"Allow While Using App\" and start tracking, exit the app so that you can be prompted to allow background tracking by selecting \"Change to Always Allow.\"")
                     Text("Alternatively, you can go to the device Settings > ATracks > Location and select \"Always\".")
+                    
                     Text("Steps Set Up")
                         .settingsSubHeader
                     Text("The steps data is obtained from the Apple Health app, and you must allow ATracks to read \"Steps.\"")
@@ -66,16 +67,24 @@ struct AboutView: View {
                 .listRowSeparator(.hidden)
                 
                 Section {
-                    Text("The app can be set up to require that you \"Start\" and \"Stop\" tracking each excursion or have the app Auto-Stop the tracking.")
-                    Text("Auto-Stop will stop the tracking if you are more than 20 yds away from your starting point more than 30 seconds after starting to track, and then return within 8 yds of your starting point.")
+                    Text("Start Tracking")
+                        .settingsSubHeader
+                    Text("Tapping on the \"Start\" button is the easiest way to start tracking your activity. An alert will ask you for a track name, and you can enter a custom name or leave it blank and the default, timestamp name will be used. You can turn off the alert in settings if you like to use the default name.")
+                    Text("You can also start a track by swiping on a track with the name that you would like to use for the new track, and then tapping on the \"timer\" icon.")
+                    
+                    Text("Stop Tracking")
+                        .settingsSubHeader
+                    Text("Auto-Stop will stop the tracking if you are more than 20 yds away from your starting point more than 30 seconds after starting to track, and then return within 8 yds of your starting point. The stop button will show \"[Stop]\" or \"[Stop Tracking]\" when auto-stop is turned on to indicate that tapping the stop button is unnecessary.")
+                    Text("Auto-Stop can be turned on and off on the in-app Settings page.")
                 } header: {
-                    Text("Tracking & Auto Stop")
+                    Text("Tracking")
                         .settingsHeader
                 }
                 .listRowSeparator(.hidden)
                 
                 Section {
                     Text("The step data is not available from the Health App immediately, so there is always a lag in the data. Final data is usually available a couple of hours after completing your activity.")
+                    Text("The Health App only runs on iPhones, so only tracks created on an iPhone will have valid step data. Also, step data can only be transferred from the Health App when ATracks is running on an iPhone.")
                 } header: {
                     Text("Steps")
                         .settingsHeader
@@ -84,7 +93,7 @@ struct AboutView: View {
                 
                 Section {
                     Text("Search tracks by track name by pulling down on the track list to reveal the search bar.")
-                    Text("The search is case-insensitive.")
+                    Text("The search is not case-sensitive.")
                     Text("Search for exact matches by placing the search text in quotes.")
                     Text("Ignore items by prefacing the search text with a minus sign.")
                 } header: {
